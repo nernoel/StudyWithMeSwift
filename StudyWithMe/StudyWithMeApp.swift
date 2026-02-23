a213c5eabd1359cceaea7efa083db24a7caa1988
nernoel <142674871+nernoel@users.noreply.github.com>
2026-02-23 18:36:47 -0500

    Initial Commit

---
+++ StudyWithMe/StudyWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyWithMeApp.swift
+//  StudyWithMe
+//
+//  Created by Noel Erulu on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct StudyWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudyWithMeTests/StudyWithMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudyWithMeTests.swift
+//  StudyWithMeTests
+//
+//  Created by Noel Erulu on 2/23/26.
+//
+
+import Testing
+@testable import StudyWithMe
+
+struct StudyWithMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
